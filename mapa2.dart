4425c9747629eac8c42094e76a6393b20a2d77da
ReyesRamirez <a.22308051281289@cbtis128.edu.mx>
2025-02-12 20:40:42 +0000

    publicar mapa2

---
+++ mapa2.dart
@@ -0,0 +1,34 @@
+void main(){
+
+    print("Reyes Alfonso Ramirez Martinez Mat 22308051281289 gpo 6 J");
+    Map<String, dynamic> Clientes = {
+        "id_cliente": 12, 
+        "nombre": "Reyes Alfonso Ramirez Martinez", 
+        "email": "a.22308051281289@cbtis128.edu.mx",
+        "telefono": 1234567890,
+        "direccion": "Calle 123, Colonia 123, Ciudad 123, Estado 123, Pais 123",
+        "activo": true,
+        "fecha_nacimiento": DateTime.parse("2007-01-06"),
+        };
+        Map<String, dynamic> Pedidos = {
+        "id_pedido": 1, 
+        "fecha_pedido": DateTime.parse("2021-10-06"), 
+        "id_cliente": "a.22308051281289@cbtis128.edu.mx",
+        "total_pedido": 47839.56,
+        "estado": "Enviado",
+        "metodo_pago": "Tarjeta",
+        "fecha_entrega": DateTime.parse("2021-10-08"),
+        };
+    print("Mapa de Clientes: ");
+    print(Clientes);
+    print("iterar un mapa con forEach: ");
+    Clientes.forEach((key, value){
+        print("$key, $value");
+    });
+
+    print("Mapa de Pedidos: ");
+    print(Pedidos);
+    for(var key in Pedidos.keys){
+        print("${Pedidos[key]}");
+    }
+}
--- recursos/mapa1.md
+++ recursos/mapa1.md
@@ -4,3 +4,6 @@ salida de datos del map
 
 otra salida usando ciclo for
 ![alt text](image-2.png)
+
+salida de tablas
+![alt text](image-4.png)
